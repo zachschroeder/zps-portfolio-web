2c2b18a0a3300f6b0efbd3a4900aa2705e49fb6a
zachschroeder <zach.schroeder17@gmail.com>
2023-06-18 13:31:20 -0400

    Added functional menu/hamburger button to navbar for mobile

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class NavigationComponent {
 
+  // Toggles navbar-burger between burger icon and 'x' icon
+  // Also shows and hides navbar-menu
+  toggleBurgerAndMenu() {
+    document.querySelector('.navbar-burger')?.classList.toggle('is-active');
+    document.querySelector('.navbar-menu')?.classList.toggle('is-active');
+  }
+
 }
