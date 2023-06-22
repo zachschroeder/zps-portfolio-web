930472e0d8ab431bf2399d696f28ab6e50009089
zachschroeder <zach.schroeder17@gmail.com>
2023-06-21 21:27:00 -0400

    Added WorkComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'work', component: WorkComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    HomeComponent
+    HomeComponent,
+    WorkComponent
   ],
   imports: [
     BrowserModule,
