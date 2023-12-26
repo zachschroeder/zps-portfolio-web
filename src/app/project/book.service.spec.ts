36b5eca1d1b24476b639b0427d78119a0d3c0d0f
Zach Schroeder <zach.schroeder17@gmail.com>
2023-12-26 16:47:01 -0500

    Added testing pattern

--- src/app/project/book.service.spec.ts
+++ src/app/project/book.service.spec.ts
@@ -1,16 +1,56 @@
 import { TestBed } from '@angular/core/testing';
-
 import { BookService } from './book.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+import { Book } from './models/book';
+
+const mockBookList: Book[] = [
+  {
+    id: '1',
+    title: 'Green Eggs and Ham',
+    author: 'Dr. Seuss',
+  },
+  {
+    id: '2',
+    title: 'The Hobbit',
+    author: 'J. R. R. Tolkien',
+  },
+];
 
 describe('BookService', () => {
   let service: BookService;
+  let httpClientSpy: jasmine.SpyObj<HttpClient>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: httpClientSpy,
+        },
+      ],
+    });
     service = TestBed.inject(BookService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getBooks$()', () => {
+    it('should return an observable of books', () => {
+      // Arrange
+      httpClientSpy.get.and.returnValue(of(mockBookList));
+
+      // Act
+      service.getBooks$().subscribe((result) => {
+        // Assert
+        expect(result.length).toEqual(mockBookList.length);
+      });
+    });
   });
 });
--- src/app/project/project.component.spec.ts
+++ src/app/project/project.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ProjectComponent } from './project.component';
+import { BookService } from './book.service';
 
 describe('ProjectComponent', () => {
   let component: ProjectComponent;
@@ -8,7 +10,9 @@ describe('ProjectComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ProjectComponent],
+      providers: [BookService],
     });
     fixture = TestBed.createComponent(ProjectComponent);
     component = fixture.componentInstance;
