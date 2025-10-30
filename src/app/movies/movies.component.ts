f3a947c049651b348b7d6bc7e84bc483aecdb22f
Zach Schroeder <zachpschroeder@pm.me>
2025-10-30 08:54:56 -0400

    Added reload button and loading animations to Movies

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,18 +1,31 @@
-import { AsyncPipe, NgForOf, NgIf } from '@angular/common';
+import { NgForOf, NgIf } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { MoviesService } from './movies.service';
 
 @Component({
   selector: 'app-movies',
   standalone: true,
-  imports: [AsyncPipe, NgForOf, NgIf],
+  imports: [NgForOf, NgIf],
   templateUrl: './movies.component.html',
   styleUrl: './movies.component.scss',
 })
 export class MoviesComponent implements OnInit {
   service = inject(MoviesService);
 
+  isLoadingGetMovies: boolean = true;
+  movies: any[] = [];
+
   ngOnInit(): void {
-    this.service.fetchMovieList();
+    this.getMovies();
+  }
+
+  getMovies(): void {
+    this.isLoadingGetMovies = true;
+    this.movies = [];
+
+    this.service.fetchMovieList().subscribe((res: any) => {
+      this.movies = res;
+      this.isLoadingGetMovies = false;
+    });
   }
 }
--- src/app/movies/movies.service.ts
+++ src/app/movies/movies.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { filter, map, Subject, take } from 'rxjs';
-import { Movie, MovieListResponse } from './movies-models';
+import { map, take } from 'rxjs';
+import { MovieListResponse } from './movies-models';
 
 @Injectable({
   providedIn: 'root',
@@ -9,20 +9,12 @@ import { Movie, MovieListResponse } from './movies-models';
 export class MoviesService {
   private http = inject(HttpClient);
 
-  private movieListSubject = new Subject<Movie[]>();
-  movieList$ = this.movieListSubject
-    .asObservable()
-    .pipe(filter((val) => !!val));
-
   fetchMovieList() {
     const route =
       'https://efhveup792.execute-api.us-east-2.amazonaws.com/default/movies';
-    return this.http
-      .get<MovieListResponse>(route)
-      .pipe(
-        take(1),
-        map((res) => res.movie_list)
-      )
-      .subscribe((res) => this.movieListSubject.next(res));
+    return this.http.get<MovieListResponse>(route).pipe(
+      take(1),
+      map((res) => res.movie_list)
+    );
   }
 }
