574ce0c5bba298e6d70811cc1caca6650a7d4494
Zach Schroeder <zachpschroeder@pm.me>
2025-11-02 19:17:31 -0500

    Got basic working copy with dummy data

--- src/app/groceries/groceries.component.ts
+++ src/app/groceries/groceries.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-groceries',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './groceries.component.html',
   styleUrl: './groceries.component.scss',
 })
-export class GroceriesComponent {}
+export class GroceriesComponent {
+  dayState: DayState = new DayState();
+
+  constructor() {
+    this.dayState.sections = [
+      new Section('Monday', ['Chicken', 'Cheese', 'Bread']),
+      new Section('Breakfast', ['Yogurt', 'Granola']),
+    ];
+  }
+
+  add(section: string, item: string) {
+    console.log(section);
+    console.log(item);
+
+    this.dayState.addItem(section, item);
+  }
+}
+
+export class DayState {
+  sections: Section[] = [];
+
+  addItem(section: string, item: string) {
+    const sectionIndex = this.sections.findIndex((s) => s.name === section);
+    if (sectionIndex !== -1) {
+      this.sections[sectionIndex].items.push(item);
+    }
+  }
+}
+
+export class Section {
+  name: string;
+  items: string[];
+
+  constructor(name: string, items: string[]) {
+    this.name = name;
+    this.items = items;
+  }
+}
