da96af8989d3556fd37848d54a6faf2718c06738
Zach Schroeder <zachpschroeder@pm.me>
2025-11-07 18:48:45 -0500

    Added checkbox and strikethrough

--- src/app/groceries/grocery-item/grocery-item.component.ts
+++ src/app/groceries/grocery-item/grocery-item.component.ts
@@ -1,11 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-grocery-item',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './grocery-item.component.html',
   styleUrl: './grocery-item.component.scss',
 })
 export class GroceryItemComponent {
   @Input({ required: true }) name: string = '';
+  isChecked: boolean = false;
 }
